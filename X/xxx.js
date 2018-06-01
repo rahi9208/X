330f0aa751f1524619c9acb52960859227dbd054
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-06-01 15:37:39 +0530

    yo

--- X/xxx.js
+++ X/xxx.js
@@ -2,5 +2,5 @@ let AWS = require('aws-sdk');
 exports.handler = function(event, context, callback) {
 
 
-	callback(null,'Successfully executed Yo'+event.message);
+	callback(null,'Successfully executed');
 }
