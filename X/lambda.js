6a8aa6a0746bc3b2b05d42e52a9adc29f5f3799d
rahi9208 <37790804+rahi9208@users.noreply.github.com>
2018-06-01 15:50:13 +0530

    yo2

--- X/lambda.js
+++ X/lambda.js
@@ -1,6 +1,17 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const ddb = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
 
+	ddb.get({
+		TableName: 'BookingInfoAnomalies',
+		Key: { 'ResellerID': 'klkl', 'BookingRef': 'kl' }
+	}, function (err, data) {
+		if (err) {
+			//handle error
+		} else {
+			//your logic goes here
+		}
+	});
 
-	callback(null,'Successfully executed Yo'+event.message);
+	callback(null, 'Successfully executed Yo' + event.message + process.env['MY_KEY']);
 }
